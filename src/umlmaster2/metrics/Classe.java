08f31d48b4ae257ec02faff543a636f6170e9cd3
Joneyviana <arcistiel@gmail.com>
2014-11-06 11:00:16 -0200

    corrigindo e adiconando DIT classe e plugin

--- src/umlmaster2/metrics/Classe.java
+++ src/umlmaster2/metrics/Classe.java
@@ -8,8 +8,9 @@ import org.jsoup.parser.Tag;
 import org.jsoup.select.Elements;
 
 public class Classe {
+private String id ;
 private String Name;
-public int DIT ;
+private  int DIT ;
 private int NOC ;
 private int CBO ;
 private static HashMap<Element,Classe> elementos_classe = new HashMap<>();
@@ -20,11 +21,8 @@ private Classe(Element ele) {
 		// TODO Auto-generated constructor stub
 	}
 public int  getNOC() {
-	String id = element.select("[xmi:type=\"uml:Generalization\"]").attr("general");
-    if (id.isEmpty()){
-    	return 0 ;
-    }
-	return element.parent().select("generalization[general="+id+"]").size();
+	
+	return element.parent().select("generalization[general="+getId()+"]").size();
 }
 public static Classe getInstanceNotEqualOther(Element ele){
 	if (elementos_classe.containsKey(ele)){
@@ -35,17 +33,34 @@ public static Classe getInstanceNotEqualOther(Element ele){
 	
 }
 public int getCBO(){
-	String id = element.attr("xmi:id");
-	int  elementos_cbo = element.parent().select("ownedAttribute[type="+id+"]").size();
 	
-	    
-		return  elementos_cbo + element.select("ownedAttribute[association]").size();
+	int  elementos_cbo = element.parent().select("ownedAttribute[type="+getId()+"]").size();
+	return  elementos_cbo + element.select("ownedAttribute[association]").size();
 }
 
 public String getName() {
 	return element.attr("name");
 }
-
-
+public String getId() {
+	return element.attr("xmi:id") ;
+}
+public int getDIT() {
+	
+	
+	return recursao_DIT(this.element) ;
+ 
+}
+private int recursao_DIT(Element element2) {
+	
+	String id = element2.select("[xmi:type=\"uml:Generalization\"]").attr("general");
+	if (id.isEmpty()){
+		return 0 ;
+	}
+	
+	Elements pai = element.parent().select("packagedElement[xmi:id="+id+"]");
+	return recursao_DIT(pai.get(0)) + 1;
+}
 
 }
+
+
